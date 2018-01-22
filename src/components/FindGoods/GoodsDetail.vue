74118bc9dfddac7f18b076badb2cdc2aaf04fc83
coolia <gj@vfu56.com>
2018-01-22 09:50:54 +0800

    cc

--- src/components/FindGoods/GoodsDetail.vue
+++ src/components/FindGoods/GoodsDetail.vue
@@ -166,9 +166,9 @@
         created() {
         	this.getGoodsDetail();
         },
-        watch:{
-	        '$route':'getGoodsDetail'
-	    },
+		watch:{
+			'$route':'getGoodsDetail'
+		},
 		methods: {
 			errorImg (e) {
                 e.target.src = defaultImg
