84ec79ad23c9170417c85574e9236acf22fb4f9d
coolia <gj@vfu56.com>
2018-01-30 17:47:42 +0800

    cccc

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 module.exports = {
 	NODE_ENV: '"production"',
-	// API_URI: '"http://api.hdd56.com/hdd/api/"',
-	// API_IMG_URI: '"http://www.hdd56.com/hdd/image/"'
-	API_URI: '"http://develop.we-service.cn/test-hdd/v2/"',
-	API_IMG_URI: '"http://develop.we-service.cn/hdd/image/"'
+	API_URI: '"http://api.hdd56.com/hdd/api/"',
+	API_IMG_URI: '"http://www.hdd56.com/hdd/image/"'
+	// API_URI: '"http://develop.we-service.cn/test-hdd/v2/"',
+	// API_IMG_URI: '"http://develop.we-service.cn/hdd/image/"'
 }
