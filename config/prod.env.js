a4b7090547220f883e41117f8d02b0e08dd0795d
Caoanlong <caoanlong@gmail.com>
2018-06-21 10:01:41 +0800

    gg

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,5 @@
 module.exports = {
 	NODE_ENV: '"production"',
-	API_URI: '"http://api.hdd56.com/hdd/api/"',
+	API_URI: '"http://api.hdd56.com/hdd/api2/"',
 	API_IMG_URI: '"http://www.hdd56.com/hdd/image/"'
-	// API_URI: '"http://develop.we-service.cn/test-hdd/v2/"',
-	// API_IMG_URI: '"http://develop.we-service.cn/hdd/image/"'
 }
