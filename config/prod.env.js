b912d4b53d7e763d93c7a06babc466dde43be4f3
Caoanlong <caoanlong@gmail.com>
2018-06-22 17:10:46 +0800

    修改url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
 	NODE_ENV: '"production"',
-	API_URI: '"http://api.hdd56.com/hdd/api2/"',
-	API_IMG_URI: '"http://www.hdd56.com/hdd/image/"'
+	API_URI: '"http://api.hdd56.com/hdd/"',
+	API_IMG_URI: '"http://image.hdd56.com/"'
 }
